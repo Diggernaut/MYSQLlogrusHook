215dddb9faa6349e27a96fdf5bc1675a165c41cb
iapon <evgeniysolomanidin@gmail.com>
2017-02-11 20:41:08 +0300

    init

--- MYSQLlogrusHook.go
+++ MYSQLlogrusHook.go
@@ -1,24 +1,16 @@
 package MYSQLlogrusHook
-
-
 import (
     "github.com/Sirupsen/logrus"
     _ "github.com/go-sql-driver/mysql"
     "database/sql"
     "fmt"
 )
-
 type hooker struct{
     db *sql.DB
     t string
 }
-
-func NewHooker(conn, table string) (*hooker, error){
-    db, err := sql.Open("mysql",conn)
-    if err != nil {
-        return nil, err
-    }
-    _, err = db.Exec("CREATE TABLE IF NOT EXISTS `"+table+"` (`id` bigint unsigned NOT NULL AUTO_INCREMENT,`level` VARCHAR(10) NOT NULL,`time` DATETIME NOT NULL,`message` LONGTEXT,PRIMARY KEY (`id`),KEY `time` (`time`),KEY `level` (`level`), FULLTEXT KEY `message` (`message`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;")
+func NewHooker(db *sql.DB, table string) (*hooker, error){
+    _, err := db.Exec("CREATE TABLE IF NOT EXISTS `"+table+"` (`id` bigint unsigned NOT NULL AUTO_INCREMENT,`level` VARCHAR(10) NOT NULL,`time` DATETIME NOT NULL,`message` LONGTEXT,PRIMARY KEY (`id`),KEY `time` (`time`),KEY `level` (`level`), FULLTEXT KEY `message` (`message`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;")
     if err != nil {
         return nil, err
     }
